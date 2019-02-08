89a846e382919ffb41ec14efde753fb729b8dd7f
Prava Dhulipalla <prava@students.olin.edu>
2019-02-08 02:58:51 -0500

    finished question 1

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -10,6 +10,17 @@ License: GNU GPLv3
 
 int var1;
 
+/* Prints address of a local variable
+   
+   varname: name of the variable
+   var: address to print out
+*/
+void printAddress(char *varname, int *var) {
+	printf("Address of %s is %p\n",varname, var);
+}
+
+/* Main function to illustrate memory allocation for variables and pointers.
+*/
 int main ()
 {
     int var2 = 5;
@@ -21,6 +32,32 @@ int main ()
     printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
     printf ("s points to %p\n", s);
+
+    /* p points to 0xb14010
+	   x points to 0xb144b0
+
+	   The heap grows up as the address for x is larger.
+    */
+    void *x = malloc(64);
+    printf ("x points to %p\n", x);
+
+    /* Address of var2 is 0x7ffe541b9db8
+	   Address of y is 0x7ffe541b9dbc
+
+	   The stack grows up as the address for y is larger.
+    */
+    int y = 10;
+    printAddress("y", &y);
+
+    /* Address of chunk1 is 0xb14500
+	   Address of chunk2 is 0xb14520
+
+	   There are 32 Bytes between them.
+    */
+    void *chunk1 = malloc(8);
+    void *chunk2 = malloc(8);
+    printAddress("chunk1", chunk1);
+    printAddress("chunk2", chunk2);
 
     return 0;
 }
